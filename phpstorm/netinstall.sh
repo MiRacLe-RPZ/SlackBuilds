aa7c412735c8ad7e4c22b8abc5ef9d2c1811e3c2
MiRacLe.RPZ <miracle@rpz.name>
2012-09-24 21:54:59 +0400

    created netinstall pseudo-packages

--- phpstorm/netinstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-set -eu
-
-cd usr/share/doc/PKGNAM-VERSION/SlackBuild/ && OUTPUT=/tmp sh ./PKGNAM.SlackBuild && /sbin/upgradepkg --install-new /tmp/PKGNAM-VERSION-ARCH-netinstall.PKGTYPE
