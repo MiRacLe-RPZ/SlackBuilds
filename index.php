d570ed3846749bdb3a904d60d925353ffd80ba21
MiRacLe.RPZ <miracle@rpz.name>
2013-08-01 22:48:32 +0400

    test auto-deploy

--- index.php
+++ index.php
@@ -3,21 +3,26 @@
     use \Michelf\Markdown;
 
     if ('POST' == $_SERVER['REQUEST_METHOD']) {
-        $is_github = function() {
-             $ip = ip2long($_SERVER['REMOTE_ADDR']);
-             foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
-                 list($subnet, $mask) = explode('/', $cidr);
-                 if (($ip & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
-                    return true;
-                 }
-             }
-             return false;
-        };
-        if ($is_github()) {
-            fastcgi_finish_request();
-            chdir(__DIR__);
-            `git pull > /dev/null 2>&1 &`;
-            exit;
+        if (false !== ($lock = fopen(__DIR__.DIRECTORY_SEPARATOR.'.lock','w+'))) {
+            if (flock($lock,LOCK_EX)) {
+                $is_github = function() {
+                    $ip = ip2long($_SERVER['REMOTE_ADDR']);
+                    foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
+                        list($subnet, $mask) = explode('/', $cidr);
+                        if (($ip & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
+                            return true;
+                        }
+                    }
+                    return false;
+                };
+                if ($is_github()) {
+                    fastcgi_finish_request();
+                    chdir(__DIR__);
+                    `git pull > /dev/null 2>&1 &`;
+                }
+                flock($lock,LOCK_UN);
+            }
+            fclose($lock);
         }
     } else {
         $text = file_get_contents('README.md');
