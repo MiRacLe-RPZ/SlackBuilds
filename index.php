e10bd0f52b84ca6ec71a9d89e348308076f4dd58
MiRacLe.RPZ <miracle@rpz.name>
2013-08-01 22:08:04 +0400

    prepare auto-deploy

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <?php
-    require 'vendor/autoload.php';
-    use \Michelf\Markdown;
-    # Read file and pass content through the Markdown parser
-    $text = file_get_contents('README.md');
-    $html = Markdown::defaultTransform($text);
+    if ('POST' == $_SERVER['REQUEST_METHOD']) {
+	$is_github = function() {
+	    foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
+		list($subnet, $mask) = explode('/', $cidr);
+		if ((ip2long($ip) & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
+	    	    return true;
+		}
+	    }
+	    return false;
+	};
+	if ($is_github()) {
+	    $payload = json_decode($_REQUEST['payload']);
+	    chdir(__DIR__);
+	    `git pull > /dev/null 2>&1 &`;
+	}
+    } else {
+        require 'vendor/autoload.php';
+	use \Michelf\Markdown;
+        # Read file and pass content through the Markdown parser
+        $text = file_get_contents('README.md');
+	$html = Markdown::defaultTransform($text);
+    }
 
 ?>
 <!DOCTYPE html>
