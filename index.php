f0d3f1169d7fe723c03ed7bda5ca5781f20efd05
MiRacLe.RPZ <miracle@rpz.name>
2013-08-01 22:38:31 +0400

    test auto-deploy

--- index.php
+++ index.php
@@ -4,18 +4,18 @@
 
     if ('POST' == $_SERVER['REQUEST_METHOD']) {
         $is_github = function() {
+             $ip = ip2long($_SERVER['REMOTE_ADDR']);
              foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
                  list($subnet, $mask) = explode('/', $cidr);
-                 if ((ip2long($ip) & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
+                 if (($ip & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
                     return true;
                  }
              }
              return false;
         };
         if ($is_github()) {
-            $payload = json_decode($_REQUEST['payload']);
-            chdir(__DIR__);
             fastcgi_finish_request();
+            chdir(__DIR__);
             `git pull > /dev/null 2>&1 &`;
             exit;
         }
