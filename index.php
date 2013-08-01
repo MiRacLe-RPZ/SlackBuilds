ad9fd4f8588aaeaecbdf73c87a360ad9c187edd9
MiRacLe.RPZ <miracle@rpz.name>
2013-08-01 22:17:44 +0400

    test auto-deploy

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
 <?php
     if ('POST' == $_SERVER['REQUEST_METHOD']) {
-	$is_github = function() {
-	    foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
-		list($subnet, $mask) = explode('/', $cidr);
-		if ((ip2long($ip) & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
-	    	    return true;
-		}
-	    }
-	    return false;
-	};
-	if ($is_github()) {
-	    $payload = json_decode($_REQUEST['payload']);
-	    chdir(__DIR__);
-	    `git pull > /dev/null 2>&1 &`;
-	}
+        $is_github = function() {
+             foreach (array('204.232.175.64/27','192.30.252.0/22') as $cidr) {
+                 list($subnet, $mask) = explode('/', $cidr);
+                 if ((ip2long($ip) & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
+                    return true;
+                 }
+             }
+             return false;
+        };
+        if ($is_github()) {
+            $payload = json_decode($_REQUEST['payload']);
+            chdir(__DIR__);
+            `git pull > /dev/null 2>&1 &`;
+        }
     } else {
         require 'vendor/autoload.php';
-	use \Michelf\Markdown;
+        use \Michelf\Markdown;
         # Read file and pass content through the Markdown parser
         $text = file_get_contents('README.md');
-	$html = Markdown::defaultTransform($text);
+        $html = Markdown::defaultTransform($text);
     }
-
 ?>
 <!DOCTYPE html>
 <html>
@@ -29,9 +28,9 @@
         <title>MiRacLe's SlackBuilds</title>
     </head>
     <body>
-	<?php
-	    # Put HTML content in the document
-	    echo $html;
-	?>
+    <?php
+        # Put HTML content in the document
+        echo $html;
+    ?>
     </body>
 </html>
