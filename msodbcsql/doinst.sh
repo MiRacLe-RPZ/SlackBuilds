8d752316ce1fed8a10d3cf229d5cb13712be1109
MiRacLe.RPZ <miracle@rpz.name>
2019-12-03 23:03:03 +0300

    ODBC Driver 17.4.2.1 for SQL Server

--- msodbcsql/doinst.sh
+++ msodbcsql/doinst.sh
@@ -1,1 +1,11 @@
-odbcinst -v -q -d -n "ODBC Driver 17 for SQL Server" >/dev/null || odbcinst -i -d -f opt/microsoft/msodbcsql/etc/odbcinst.ini
+ALREADY_INSTALLED=$(odbcinst -v -q -d -n "ODBC Driver 18 for SQL Server"|grep -c "Driver")
+ALREADY_INSTALLED_VERSION=$(odbcinst -v -q -d -n "ODBC Driver 18 for SQL Server"|grep -c "17.4.so.2.1")
+
+if [ $ALREADY_INSTALLED = "1" ]; then
+    if [ $ALREADY_INSTALLED_VERSION = "0" ]; then
+	odbcinst -u -n "ODBC Driver 18 for SQL Server"
+	odbcinst -i -d -f /opt/microsoft/msodbcsql/etc/odbcinst.ini
+    fi
+else 
+    odbcinst -i -d -f /opt/microsoft/msodbcsql/etc/odbcinst.ini
+fi
