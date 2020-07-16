8824e4618edf93616f259aca6e86fa73763b6eee
MiRacLe.RPZ <miracle@rpz.name>
2020-07-16 21:47:06 +0300

    ~

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
     require 'vendor/autoload.php';
-    use \Michelf\Markdown;
+    use Michelf\Markdown;
 
     if ('POST' == $_SERVER['REQUEST_METHOD']) {
         fastcgi_finish_request();
-        if (false !== ($lock = fopen(__DIR__.DIRECTORY_SEPARATOR.'.lock','w+'))) {
+        if (false !== ($lock = fopen(__DIR__ . DIRECTORY_SEPARATOR .'.lock','w+'))) {
             if (flock($lock,LOCK_EX)) {
-                $is_github = function() {
+                $is_github = static function() {
                     $ip = ip2long($_SERVER['REMOTE_ADDR']);
                     foreach (array('192.30.252.0/22','185.199.108.0/22','140.82.112.0/20') as $cidr) { // https://api.github.com/meta
                         list($subnet, $mask) = explode('/', $cidr);
-                        if (($ip & ~((1 << (32 - $mask)) - 1) ) == ip2long($subnet)) { 
+                        if (($ip & ~((1 << (32 - $mask)) - 1) ) === ip2long($subnet)) {
                             return true;
                         }
                     }
@@ -42,7 +42,7 @@
          body {background-color: #eee;}
          span.pkgnam {font-weight: bold;}
          span.version {color: gray;}
-         a.home { background-image: url('http://icons.iconarchive.com/icons/artua/mac/16/Home-icon.png'); background-position: 0 0; background-repeat: no-repeat; color: transparent; width: 20px; display: inline-block; }
+         a.home { background-image: url('//icons.iconarchive.com/icons/artua/mac/16/Home-icon.png'); background-position: 0 0; background-repeat: no-repeat; color: transparent; width: 20px; display: inline-block; }
         </style>
     </head>
     <body>
